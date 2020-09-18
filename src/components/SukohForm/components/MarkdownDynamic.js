4cd8464b4ed386629705ebd1cfe77ace1e340c4c
Pim Snel <pim@lingewoud.nl>
2020-09-19 01:58:54 +0200

    bring back tip functionality

--- src/components/SukohForm/components/MarkdownDynamic.js
+++ src/components/SukohForm/components/MarkdownDynamic.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Tip from '../../Tip';
+import FormItemWrapper from './shared/FormItemWrapper';
 import DefaultWrapper from './shared/DefaultWrapper';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
@@ -142,31 +143,33 @@ class MarkdownDynamic extends BaseDynamic<MarkdownDynamicField, MarkdownDynamicS
             return (null);
         }
 
-        let tip = undefined;
         let paddingRight = 0;
-        if(field.tip){
-            tip = <Tip horizontalAlign='left' markdown={field.tip} style={{position:'absolute', right:0, top:20, zIndex:2}} />;
-            paddingRight = 50;
-        }
         let multiLine = field.multiLine===undefined?true:field.multiLine;
-
+        let iconButtons = [];
+        if(field.tip) iconButtons.push(<Tip markdown={field.tip} />);
 
         return (
             <div
                 ref={(div) => { this.inputWrapper = div; }}
                 style={{ width:'100%' }}
             >
-                <TextField
-                    onChange={this.onChange.bind(this)}
-                    value={this.state.value}
-                    floatingLabelFixed={true}
-                    multiLine={multiLine}
-                    fullWidth={true}
-                    rowsMax={30}
-                    underlineShow={true}
-                    textareaStyle={{minHeight: '80px'}}
-                    style={{paddingRight, transition:'none', boxSizing:'border-box'}}
-                    floatingLabelText={field.title} />
+
+            <FormItemWrapper
+            control={<TextField
+            onChange={this.onChange.bind(this)}
+            value={this.state.value}
+            floatingLabelFixed={true}
+            multiLine={multiLine}
+            fullWidth={true}
+            rowsMax={30}
+            underlineShow={true}
+            textareaStyle={{minHeight: '80px'}}
+            style={{paddingRight, transition:'none', boxSizing:'border-box'}}
+            floatingLabelText={field.title} />
+            }
+            iconButtons={iconButtons}
+        />
+
             </div>
         );
     }
