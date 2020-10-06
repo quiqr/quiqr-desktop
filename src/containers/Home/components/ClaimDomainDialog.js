4eef2a4340945a53f5dc55227548fee5275a6878
Pim Snel <pim@lingewoud.nl>
2020-10-06 17:15:04 +0200

    claiming almost finished

---
+++ src/containers/Home/components/ClaimDomainDialog.js
@@ -0,0 +1,219 @@
+import * as React from 'react';
+import Spinner from './../../../components/Spinner'
+import service from './../../../services/service';
+import { Dialog, FlatButton, MenuItem, SelectField, TextField } from 'material-ui';
+import type { SiteConfig, WorkspaceHeader, WorkspaceConfig } from './../../../types';
+import { Accordion, AccordionItem } from './../../../components/Accordion';
+import IconNavigationCheck from 'material-ui/svg-icons/navigation/check';
+import IconHttps from 'material-ui/svg-icons/action/https';
+import Paper from 'material-ui/Paper';
+//import FormData, {getHeaders} from 'form-data';
+//import FormData from 'form-data';
+let net = window.require('electron').remote.net;
+
+export default class ClaimDomainDialog extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            pogourl: "test3334",
+            email: "test@lkjflsdk.nl",
+            pogourl_err: "",
+            email_err: "",
+            failure: false,
+            busy: false,
+            username: this.props.username
+         }
+    }
+
+    handleCancelClick = () => {
+        this.props.onCancelClick();
+    }
+
+    handleDomainClaimClick = async (context) => {
+
+        this.setState({
+            busy: true
+        });
+
+        service.api.logToConsole(this.props)
+
+        this.registerDomain(this.state.pogourl, this.props.username);
+    }
+
+    registerDomain(pogourl, username){
+        service.api.logToConsole(pogourl + username)
+        if(username===""){
+            this.setState({
+                failure: true
+            });
+
+            this.setState({ busy: false });
+            return
+        }
+        var postData = JSON.stringify({sitename : pogourl, username: username});
+
+        let data='';
+        const request = net.request({
+            method: 'POST',
+            protocol: 'http:',
+            hostname: 'localhost',
+            port: 9999,
+            path: '/site/new',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': postData.length
+            }
+        })
+
+        request.on('response', (response) => {
+
+            response.on('end', () => {
+                let obj = JSON.parse(data);
+                service.api.logToConsole(obj);
+                if(obj.hasOwnProperty('path')){
+
+                    let promise = service.api.createPogoDomainConf(obj);
+                    promise.then(()=>{
+                        this.props.onClaimDomainClick({
+                            pogourl: obj.path
+                        });
+                    });
+
+
+                }
+                else{
+                    this.setState({
+                        failure: true
+                    });
+                }
+                //service.api.logToConsole(""+response.code);
+
+                this.setState({ busy: false });
+            });
+
+            response.on("data", chunk => {
+                data += chunk;
+            });
+
+        })
+        request.write(postData)
+        request.end()
+
+    }
+
+    handlepogourlChange(e){
+
+        let value = e.target.value;
+
+        if(value!==''){
+
+            let url = "https://board.poppygo.io/stat/site/"+value;
+            let data='';
+
+            const request = net.request(url);
+            request.on('response', (response) => {
+
+                response.on('end', () => {
+                    service.api.logToConsole(data);
+                    let obj = JSON.parse(data);
+
+                    if(obj.status !== "free"){
+                        this.setState({
+                            pogourl_err: "pogourl is "+obj.status
+                        });
+                    }
+                    else{
+                        this.setState({
+                            pogourl_err: ""
+                        });
+                    }
+
+                    service.api.logToConsole(obj);
+                });
+                response.on("data", chunk => {
+                    data += chunk;
+                });
+            })
+            request.end()
+        }
+
+        this.setState({
+            pogourl: value,
+        });
+
+    }
+
+    handleTryAgain(){
+        this.setState({
+            pogourl: "",
+            email: "",
+            busy: false,
+            failure: false,
+        });
+
+    }
+
+    validate(){
+        return !this.state.busy &&
+            !this.state.failure &&
+            this.state.pogourl_err === '' &&
+            this.state.pogourl !== ''
+    }
+
+    renderForm(){
+        let valid = this.validate();
+        let busy = this.state.busy;
+        let previewUrl = this.state.pogourl.replace(/\./g,"-");
+        return (
+            <div>
+                <TextField disabled={busy} errorText={this.state.pogourl_err} floatingLabelText={'PoppyGo Domain'} value={this.state.pogourl} onChange={(e)=>{this.handlepogourlChange(e)}} fullWidth />
+                <Paper style={{margin:10,padding:"0 7px 7px 7px"}}>
+                    <IconHttps viewBox="-5 0 35 10" color="#666" /><span>https://</span><span style={{color:"green"}}>{previewUrl}</span><span>.pogosite.com</span>
+                </Paper>
+            </div>
+        )
+
+    }
+
+    renderFailure(){
+        return (
+                <div>
+                    Something went wrong. Please <a href="#" onClick={()=>this.handleTryAgain()}>try again.</a>
+                </div>
+        )
+    }
+
+    render(){
+        let { open } = this.props;
+        let valid = this.validate();
+        let busy = this.state.busy;
+        let failure = this.state.failure;
+
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={false}
+                onClick={this.handleCancelClick.bind(this)}
+            />,
+            <FlatButton
+                disabled={!valid}
+                label="Claim domain"
+                primary={true}
+                onClick={this.handleDomainClaimClick}
+            />,
+        ];
+
+        return (
+            <Dialog
+                title="Claim your PoppyGo domain for easy publishing to the web"
+                open={open}
+                actions={actions}>
+
+                { failure? this.renderFailure() : this.renderForm() }
+                { busy? <Spinner /> : undefined }
+            </Dialog>
+        );
+    }
+
+}
