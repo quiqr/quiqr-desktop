5bc29bcae8f3d833c7939f6522ccdb0bb916d95d
Pim Snel <pim@lingewoud.nl>
2020-11-15 19:52:52 +0100

    debug info

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
-notarize({
+var notconf = {
     appBundleId: 'com.lingewoud.poppygo',
     appPath: 'dist/mac/PoppyGo.app',
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
-});
+}
+console.log(notconf);
+notarize(notconf);
 
 
