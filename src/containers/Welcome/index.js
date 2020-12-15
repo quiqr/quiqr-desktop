8d691c82aab3b9c5cb53d0a7bcb09cc55410f1fb
Brecht <swanenberg@gmail.com>
2020-12-15 12:31:28 +0100

    branch 216-improve-welcome-screen-for-onboarding

--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import service from './../../services/service'
+import service from './../../services/service';
+import CreateSiteDialog from '../SelectSite/components/CreateSiteDialog';
 
 //import Checkbox from 'material-ui/Checkbox';
 import { RaisedButton } from 'material-ui/';
@@ -21,15 +22,22 @@ class Welcome extends React.Component {
     componentWillMount(){
     }
 
+
+
     handleLinkThemeGallery = ()=>{
         window.require('electron').shell.openExternal("https://router.poppygo.app/theme-gallery");
     }
     handleLinkPoppyWebsite = ()=>{
-        window.require('electron').shell.openExternal("https://poppygo.io");
+        window.require('electron').shell.openExternal("https://poppygo.io/documentation/");
     }
     handleImportClick = ()=>{
         service.api.importSite();
     }
+
+    handleNewSiteClick = ()=>{
+        this.history.push('/');
+    }
+
     handleCloseClick = ()=>{
         this.history.push('/');
     }
@@ -59,24 +67,30 @@ class Welcome extends React.Component {
                                 <li>Publish with a single click</li>
                                 <li>All without setup</li>
                             </ul>
-                            <br/>
+                            <button className="reglink" onClick={this.handleLinkPoppyWebsite} >PoppyGo Docs</button>
                         </p>
-
-                        <h2>Useful links</h2>
-                        <ul>
-                            <li>
-                                <button className="reglink" onClick={this.handleLinkPoppyWebsite} >PoppyGo Website</button>
-                            </li>
-                        </ul>
-                        <h2>Start right-away</h2>
+                        <br/>
+                        <strong><p>Did you receive a pogosite file?</p></strong>
                         <p>
-                            Import the site that your developer has created for you.
+                            Then import the site that your developer has created for you.
                             Your developer didn’t send you a site yet? Check the example template to start playing around.
                         </p>
                         <p>
-                            <RaisedButton primary={true} label="Import your site right now" disabled={false} onClick={this.handleImportClick} />
+                            <RaisedButton primary={true} label="Import file" disabled={false} onClick={this.handleImportClick} />
                         </p>
-
+                        <br/>
+                        <strong><p>Are you developing a Hugo site on your local machine?</p></strong>
+                        <p>
+                          Then open your existing site folder in PoppyGo to start content management and publishing right away.
+                        </p>
+                        <p>
+                          <RaisedButton primary={true} label="Open site" disabled={false} onClick={this.handleNewSiteClick} />
+                        </p>
+                        <br/>
+                        <strong><p>Are you new to Hugo sites, and do you want to start with an existing template?</p></strong>
+                          <p>
+                            Then select a template below to start and experience PoppyGo right away.
+                          </p>
                         <object data="https://poppygo.io/themes/iframe.html" width="100%" height="3000px" scroll="no" type="text/html"></object>
 
                         <p>
