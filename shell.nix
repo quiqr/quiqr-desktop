28d72eac3f03fdc73514d34337d0a2c19a4aaf2a
Pim Snel <post@pimsnel.com>
2021-10-14 12:17:18 +0200

    fix insecure electron 5

--- shell.nix
+++ shell.nix
@@ -1,17 +1,20 @@
-with import <nixpkgs> {};
+{}:
 
+let
+  config = rec {
+    allowUnfree = true;
+    allowBroken = true;
+    permittedInsecurePackages = [ "electron-5.0.13" ];
+  };
+  pkgs = import <nixpkgs> { inherit config; };
+in
+  pkgs.mkShell {
 
+    permittedInsecurePackages = [ "electron-5.0.13" ];
 
-mkShell {
-
-  permittedInsecurePackages = [
-    "electron-5.0.13"
-  ];
-
-
-  nativeBuildInputs = [
-    nodejs
-    electron_5
-  ];
-   ELECTRON_OVERRIDE_DIST_PATH = "${electron_5}/bin/";
-}
+    nativeBuildInputs = [
+      pkgs.nodejs
+      pkgs.electron_5
+    ];
+    ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron_5}/bin/";
+  }
