2a012eebed8de513772c9450fcb212dc7d432bb5
Pim Snel <post@pimsnel.com>
2021-10-29 10:54:16 +0200

    intial working runner for embgit

--- scripts/embgit-sh-runner.js
+++ scripts/embgit-sh-runner.js
@@ -1,4 +1,22 @@
+const { exec } = require("child_process");
+const prefixlog = "embgit.sh -> ";
+let command;
 
-console.log(process.platform);
-//var isWin = process.platform === "win32";
+if(process.platform === "win32"){
+  command = "c:\\Program Files\\Git\\bin\\bash.exe", ".\\scripts\\embgit.sh -d -p"
+}
+else{
+  command = "./scripts/embgit.sh -d -p"
+}
 
+exec(command, async (error, stdout, stderr) => {
+  if (error) {
+    console.log(`${prefixlog}error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`${prefixlog}stderr: ${stderr}`);
+    return;
+  }
+  console.log(`${prefixlog}stdout: ${stdout}`);
+});
