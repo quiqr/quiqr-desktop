d92806751a9de6f9e0ff74dca5d06d64497a0ac2
Pim Snel <post@pimsnel.com>
2021-12-02 21:40:01 +0100

    reindent cleanup

--- src-main/jobs/create-thumbnail-job.js
+++ src-main/jobs/create-thumbnail-job.js
@@ -3,30 +3,33 @@ const jimp = require('jimp');
 const path = require('path');
 
 const action  = async ({src , dest}) => {
-    await fs.ensureDir(path.dirname(dest));
-    console.log ("thumbnailJob");
-    let resizePromise = new Promise((resolve, reject)=>{
-        jimp.read(src, function (err, lenna) {
-            if (err) reject(err);
-            else{
-                lenna.scaleToFit(400,400).write(dest, (err) =>{
-                    if(err) reject();
-                    else resolve();
-                });
-            }
+  await fs.ensureDir(path.dirname(dest));
+
+
+
+  console.log ("thumbnailJob");
+  let resizePromise = new Promise((resolve, reject)=>{
+    jimp.read(src, function (err, lenna) {
+      if (err) reject(err);
+      else{
+        lenna.scaleToFit(400,400).write(dest, (err) =>{
+          if(err) reject();
+          else resolve();
         });
+      }
     });
+  });
 
-    await resizePromise;
+  await resizePromise;
 
-    let thumbExistsPromise = new Promise((resolve, reject)=>{
-        fs.exists(dest,(exists)=> resolve(exists));
-    });
+  let thumbExistsPromise = new Promise((resolve, reject)=>{
+    fs.exists(dest,(exists)=> resolve(exists));
+  });
 
-    let thumbExists = await thumbExistsPromise;
-    if(!thumbExists){
-        throw new Error('Something went wrong');
-    }
+  let thumbExists = await thumbExistsPromise;
+  if(!thumbExists){
+    throw new Error('Something went wrong');
+  }
 }
 
 module.exports = action;
