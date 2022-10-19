e3409ea0ed41c49e53c8d8fea5aee90a07b30298
Pim Snel <post@pimsnel.com>
2022-10-19 11:58:33 +0200

    add new hugo config reader class

---
+++ src-main/hugo/hugo-config.js
@@ -0,0 +1,38 @@
+const { spawn }  = require('child_process');
+const spawnAw                                   = require('await-spawn')
+const fs         = require('fs-extra');
+const pathHelper = require('./../utils/path-helper');
+
+global.currentServerProccess = undefined;
+let mainWindow;
+
+class HugoConfig {
+
+  constructor(config){
+    this.config = config;
+  }
+
+  async configLines(){
+    let {config, workspacePath, hugover} = this.config;
+    const exec = pathHelper.getHugoBinForVer(hugover);
+
+    return new Promise( async (resolve, reject)=>{
+      try {
+        if(!fs.existsSync(exec)){
+          callback(new Error('Could not find hugo.exe for version '+ hugover));
+          return;
+        }
+
+        const output = await spawnAw( exec, [ "config" ], { cwd: workspacePath });
+        const lines = output.toString().split('\n')
+        resolve(lines);
+      } catch (e) {
+        console.log(e)
+        reject(e);
+      }
+    });
+
+  }
+}
+
+module.exports = HugoConfig;
