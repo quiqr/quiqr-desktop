6b0bf6451a2a784c45a02a110872e452be47dd73
Pim Snel <post@pimsnel.com>
2023-06-01 23:24:37 +0200

    add function to get assets with versionnrs

--- scripts/getHugoVersions.js
+++ scripts/getHugoVersions.js
@@ -10,10 +10,29 @@ async function getRemoteReleases(username, repository, page) {
   json = json.map(obj => obj.name);
   return json;
 }
+async function getRemoteReleasesAndAssets(username, repository, page) {
+  if(!username) throw new TypeError("Username was not provided (first parameter), received " + username);
+  if(!repository) throw new TypeError("Repository was not provided (second parameter), received " + repository);
+  let json = await fetch(`https://api.github.com/repos/${username}/${repository}/releases?per_page=100&page=${page}`).then(res => res.json());
+  if(json.message) throw new TypeError("User or repository was not found");
 
+  let dict = {};
+  json.forEach((obj)=>{
+
+    let downloads = [];
+    obj.assets.forEach((asset)=>{
+      downloads.push(asset.browser_download_url);
+    });
+
+    dict[obj.name] = downloads;
+  })
+
+  return dict;
+}
 async function run(useLocalJson){
 
   let allVersions = [];
+  let allVersionsDict = {};
   let featureVersions = {};
   let filteredVersions = [];
 
@@ -23,16 +42,20 @@ async function run(useLocalJson){
   }
   else{
     let versions = [];
+    let versionsDict = {};
     let i=1;
     while(true){
       versions = await getRemoteReleases("gohugoio", "hugo", i);
+      //versionsDict = await getRemoteReleasesAndAssets("gohugoio", "hugo", i);
       i++;
       if(versions.length === 0) break;
       allVersions = allVersions.concat(versions);
+      //allVersionsDict = Object.assign(allVersionsDict, versionsDict);
     }
 
     try {
       fs.writeFileSync('resources/all/allVersions.json', JSON.stringify(allVersions));
+      //fs.writeFileSync('resources/all/allVersionsDict.json', JSON.stringify(allVersionsDict));
     } catch (error) {
       console.error(err);
     }
