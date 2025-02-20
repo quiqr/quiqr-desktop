239b2e6f4444875c771f4b4ead2a96dae3021e77
Pim Snel <post@pimsnel.com>
2025-02-20 20:08:15 +0000

    build script for on distrobox

--- .distrobox.sh
+++
@@ -1,6 +0,0 @@
-echo "This is made for distrobox with ubuntu 24.04"
-npm install
-npm install @kurkle/color
-NODE_OPTIONS=--openssl-legacy-provider npm run build
-npm exec electron-builder
-cp dist/quiqr_0.19.4_linux_x86_64.AppImage /tmp
