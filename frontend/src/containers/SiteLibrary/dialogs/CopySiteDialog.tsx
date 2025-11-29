6714004aefc6450fbc4a3eb83c66229954243874
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-11-29 15:27:16 +0100

    fix: CopySiteDialog

--- frontend/src/containers/SiteLibrary/dialogs/CopySiteDialog.tsx
+++ frontend/src/containers/SiteLibrary/dialogs/CopySiteDialog.tsx
@@ -7,7 +7,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { SiteConfig } from "../../../../types";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState } from "react";
 
 interface CopyDialogProps {
   open: boolean;
@@ -20,14 +20,12 @@ interface CopyDialogProps {
 const CopyDialog = ({ open, siteconf, localsites, onSavedClick, onCancelClick }: CopyDialogProps) => {
   const [errorTextSiteName, setErrorTextSiteName] = useState("");
 
+  console.log(siteconf);
+
   // Initialize state with the copied siteconf (computed during state initialization)
   const [editedSiteConf, setEditedSiteConf] = useState(() => (siteconf.key ? { ...siteconf, name: siteconf.name + " (copy)" } : { key: "", name: "" }));
 
   const execButtonsDisabled = !siteconf.key;
-
-  useEffect(() => {
-    console.log(siteconf);
-  }, []);
 
   const validateSiteName = useCallback(
     (newName: string) => {
@@ -43,7 +41,7 @@ const CopyDialog = ({ open, siteconf, localsites, onSavedClick, onCancelClick }:
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newName = e.target.value;
-    setEditedSiteConf({ ...editedSiteConf, name: newName });
+    setEditedSiteConf({ ...siteconf, name: newName, key: newName });
     validateSiteName(newName);
   };
 
@@ -52,7 +50,7 @@ const CopyDialog = ({ open, siteconf, localsites, onSavedClick, onCancelClick }:
       return;
     }
 
-    await service.api.copySite(editedSiteConf.key, editedSiteConf);
+    await service.api.copySite(siteconf.key, editedSiteConf);
     onSavedClick();
   };
 
@@ -65,7 +63,7 @@ const CopyDialog = ({ open, siteconf, localsites, onSavedClick, onCancelClick }:
             id='standard-full-width'
             label='Site Name'
             fullWidth
-            value={editedSiteConf?.name || ""}
+            value={editedSiteConf?.name || siteconf.name || ""}
             onChange={handleNameChange}
             error={errorTextSiteName !== ""}
             helperText={errorTextSiteName}
