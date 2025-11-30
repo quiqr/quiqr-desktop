11e743f9d0545b16eeeb2e90140c683c3096b5c7
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-11-30 14:53:46 +0100

    fix: change ReactDOM.render to createRoot

--- frontend/src/console.tsx
+++ frontend/src/console.tsx
@@ -1,30 +1,17 @@
-import React             from 'react';
-import ReactDOM          from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import App               from './App';
-import service           from './services/service';
 import SnackbarManager   from './components/SnackbarManager';
-import { UserPreferences } from '../types';
-
-service.api.readConfKey('prefs').then((value: UserPreferences)=>{
-
-  let appUiStyle = 'quiqr10';
-
-  require('./app-ui-styles/' + appUiStyle + '/css/index.css');
-  require('./app-ui-styles/' + appUiStyle + '/css/bootstrap-grid.css');
-
-  /* STYLES FOR OTHER THEN MUI COMPONENTS */
-  require('./app-ui-styles/components.css');
-});
 
 console.log("CONSOLE!")
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <BrowserRouter>
     <div>
       <SnackbarManager />
       <App />
     </div>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
-import React             from 'react';
 import { createRoot }    from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import App               from './App.jsx';
-import service           from './services/service';
 import SnackbarManager   from './components/SnackbarManager.jsx';
 
 // Import theme CSS files
