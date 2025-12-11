30b40b9026ac54f5f667f01956f3984428afec18
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-12-11 13:04:05 +0100

    convert CardNew to functional

--- frontend/src/containers/WorkspaceMounted/Sync/syncTypes/sysgit/CardNew.tsx
+++ frontend/src/containers/WorkspaceMounted/Sync/syncTypes/sysgit/CardNew.tsx
@@ -1,8 +1,7 @@
-import * as React          from 'react';
-import Box                 from '@mui/material/Box';
-import Paper               from '@mui/material/Paper';
-import Typography          from '@mui/material/Typography';
-import GitHubIcon          from '@mui/icons-material/GitHub';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 interface CardNewProps {
   handleClick: () => void;
@@ -12,33 +11,25 @@ interface CardNewProps {
   };
 }
 
-class CardNew extends React.Component<CardNewProps> {
-
-  render(){
-    return (
-      <Paper
-        onClick={()=>{
-          this.props.handleClick();
-        }}
-        className={this.props.classes?.paper}
-        sx={{ cursor: 'pointer', padding: 2 }}
-        elevation={5}
-      >
-        <Box display="flex" alignItems="center" justifyContent="center" height={63}>
-          <GitHubIcon fontSize="large" />
-        </Box>
-        <Box display="flex" alignItems="center" justifyContent="center" >
-          <Typography variant="h5">Git Target</Typography>
-        </Box>
-        <Box display="flex" textAlign="center">
-          <Typography variant="body2">Sync with git server using system git binary</Typography>
-        </Box>
-
-
-      </Paper>
-    )
-  }
-
+function CardNew({ handleClick, classes }: CardNewProps) {
+  return (
+    <Paper
+      onClick={handleClick}
+      className={classes?.paper}
+      sx={{ cursor: 'pointer', padding: 2 }}
+      elevation={5}
+    >
+      <Box display="flex" alignItems="center" justifyContent="center" height={63}>
+        <GitHubIcon fontSize="large" />
+      </Box>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Typography variant="h5">Git Target</Typography>
+      </Box>
+      <Box display="flex" textAlign="center">
+        <Typography variant="body2">Sync with git server using system git binary</Typography>
+      </Box>
+    </Paper>
+  );
 }
 
 export default CardNew;
