f4ea97bf86714ebdce504efcaf7eba515334cc0a
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2025-12-11 14:04:20 +0100

    convert collection action dialogs to functional

--- frontend/src/containers/WorkspaceMounted/Collection/DeleteItemKeyDialog.tsx
+++ frontend/src/containers/WorkspaceMounted/Collection/DeleteItemKeyDialog.tsx
@@ -1,71 +1,52 @@
-import * as React        from 'react';
-import Button            from '@mui/material/Button';
-import DialogTitle       from '@mui/material/DialogTitle';
-import Dialog            from '@mui/material/Dialog';
-import DialogActions     from '@mui/material/DialogActions';
-import DialogContent     from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import Spinner           from './../../../components/Spinner'
+import Spinner from './../../../components/Spinner';
 
 interface DeleteItemKeyDialogProps {
   busy?: boolean;
   itemLabel: string;
   handleClose?: () => void;
-  handleConfirm?: (value: string) => void;
+  handleConfirm?: () => void;
 }
 
-interface DeleteItemKeyDialogState {
-  value: string;
-  valid: boolean | null;
-}
-
-class DeleteItemKeyDialog extends React.Component<DeleteItemKeyDialogProps, DeleteItemKeyDialogState>{
-  constructor(props){
-    super(props);
-    this.state = {
-      value:'',
-      valid: null
+function DeleteItemKeyDialog({ busy, itemLabel, handleClose, handleConfirm }: DeleteItemKeyDialogProps) {
+  const onClose = () => {
+    if (handleClose && !busy) {
+      handleClose();
     }
-  }
+  };
 
-  handleClose(){
-    if(this.props.handleClose && !this.props.busy)
-      this.props.handleClose();
-  }
+  const onConfirm = () => {
+    if (handleConfirm) {
+      handleConfirm();
+    }
+  };
 
-  handleConfirm(){
-    if(this.props.handleConfirm)
-      this.props.handleConfirm(this.state.value);
-  }
-
-  render(){
-    const { busy, itemLabel } = this.props;
-
-    return (
-      <Dialog fullWidth={true} maxWidth='sm' open={true} onClose={this.handleClose}>
-        <DialogTitle>Delete Item</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {this.state.valid ? undefined : (
-              <div>
-                Do you really want to delete <b>"{itemLabel}"</b>?
-              </div>
-            )}
-
-            {busy ? <Spinner /> : undefined}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button disabled={busy} onClick={this.handleClose.bind(this)} color='primary'>
-            Cancel
-          </Button>
-          <Button disabled={busy} onClick={this.handleConfirm.bind(this)} color='primary'>
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+  return (
+    <Dialog fullWidth={true} maxWidth="sm" open={true} onClose={onClose}>
+      <DialogTitle>Delete Item</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <div>
+            Do you really want to delete <b>"{itemLabel}"</b>?
+          </div>
+          {busy && <Spinner />}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button disabled={busy} onClick={onClose} color="primary">
+          Cancel
+        </Button>
+        <Button disabled={busy} onClick={onConfirm} color="primary">
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
-export default DeleteItemKeyDialog
+export default DeleteItemKeyDialog;
