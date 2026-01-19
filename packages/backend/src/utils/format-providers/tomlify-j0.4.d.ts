0d2f86d26189fb49fc0e72ef0ab5912e5c969cec
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2026-01-19 21:57:10 +0100

    replace any with unknown in tomlify package

--- packages/backend/src/utils/format-providers/tomlify-j0.4.d.ts
+++ packages/backend/src/utils/format-providers/tomlify-j0.4.d.ts
@@ -6,10 +6,10 @@ declare module 'tomlify-j0.4' {
   export interface TomlifyOptions {
     space?: number;
     sort?: boolean;
-    replace?: (key: string, value: any) => any;
+    replace?: (key: string, value: unknown) => any;
   }
 
-  export function toToml(obj: any, options?: TomlifyOptions): string;
+  export function toToml(obj: unknown, options?: TomlifyOptions): string;
 
   const tomlify: {
     toToml: typeof toToml;
