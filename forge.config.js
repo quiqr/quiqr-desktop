3f20010259b6dba249084dcd655fe92c51866386
Bert-Jan Zwanepol <bert-janzwanepol@hotmail.com>
2026-02-03 16:37:53 +0100

    change git repo url in forge config too and disable mac and windows builds

--- forge.config.js
+++ forge.config.js
@@ -180,7 +180,7 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'quiqr',
+          owner: 'bert-janzwanepol',
           name: 'quiqr-desktop',
         },
         prerelease: false,
@@ -190,22 +190,24 @@ module.exports = {
   ],
 
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        name: 'Quiqr',
-        authors: 'Quiqr Organization',
-        description: 'Local-first CMS for static files and engines like Quarto or Hugo SSG',
-        setupIcon: 'packages/frontend/public/icon.ico',
-        iconUrl: 'https://raw.githubusercontent.com/quiqr/quiqr-desktop/main/packages/frontend/public/icon.ico',
-        loadingGif: undefined,
-        noMsi: true,
-      },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
+    // Temporarily disabled for testing - Windows build
+    // {
+    //   name: '@electron-forge/maker-squirrel',
+    //   config: {
+    //     name: 'Quiqr',
+    //     authors: 'Quiqr Organization',
+    //     description: 'Local-first CMS for static files and engines like Quarto or Hugo SSG',
+    //     setupIcon: 'packages/frontend/public/icon.ico',
+    //     iconUrl: 'https://raw.githubusercontent.com/quiqr/quiqr-desktop/main/packages/frontend/public/icon.ico',
+    //     loadingGif: undefined,
+    //     noMsi: true,
+    //   },
+    // },
+    // Temporarily disabled for testing - macOS build
+    // {
+    //   name: '@electron-forge/maker-zip',
+    //   platforms: ['darwin'],
+    // },
     {
       name: '@electron-forge/maker-deb',
       config: {
