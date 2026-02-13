d464fc079dede874d76c55d7c42f6209fe07cb08
Pim Snel <post@pimsnel.com>
2026-02-13 13:41:24 +0100

    fix missing coverage-summary

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      reporter: ['text', 'json-summary', 'json'],
+      //provider: 'istanbul'
+    },
     projects: [
       './packages/types/vitest.config.ts',
       './packages/backend/vitest.config.ts',
